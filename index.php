9a9b394c9a41b528dff1f4684265a056d9d31bb6
engwebdev <eng.web.dev.mmm@gmail.com>
2024-03-02 23:49:37 +0330

    06 -> the project is functionally finished. But it needs to clean up the code as well as classes and etc...

--- index.php
+++ index.php
@@ -44,3 +44,27 @@ $select_player_1 = \cli\menu($vehicleChoices, null, "{$pleyer_one}, please choos
 $select_player_2 = \cli\menu($vehicleChoices, null, "{$pleyer_one}, please choose your vehicle:");
 echo "player " . $pleyer_one . " select " . $vehicles[$select_player_1]->name . "\n";
 echo "player " . $pleyer_two . " select " . $vehicles[$select_player_2]->name . "\n";
+//var_dump($vehicles[$select_player_2]);
+if($vehicles[$select_player_2]->unit != 'Km/h'){
+    $speed_2 = $vehicles[$select_player_2]->maxSpeed * 1.852;
+    echo $speed_2 . " convert from " . $vehicles[$select_player_2]->unit . "\n";
+}else{
+    $speed_2 = $vehicles[$select_player_2]->maxSpeed;
+    echo $speed_2 . " from " . $vehicles[$select_player_2]->unit . "\n";
+}
+if($vehicles[$select_player_1]->unit != 'Km/h'){
+    $speed_1 = $vehicles[$select_player_1]->maxSpeed * 1.851;
+    echo $speed_1 . " convert from " . $vehicles[$select_player_1]->unit . "\n";
+}else{
+    $speed_1 = $vehicles[$select_player_1]->maxSpeed;
+    echo $speed_1 . " from " . $vehicles[$select_player_1]->unit . "\n";
+}
+
+echo "finally.... " . "\n";
+if ($speed_1 > $speed_2){
+    echo "player " . $pleyer_one . " is winner!! " . "\n";
+}elseif ($speed_1 < $speed_2){
+    echo "player " . $pleyer_two . " is winner!! " . "\n";
+}else{
+    echo "we do not have winner! every player lost..... " . "\n";
+}
